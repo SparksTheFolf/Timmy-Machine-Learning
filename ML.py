4e76f2dc9926d802ca1cba51e8612cf406ffcbb4
nolant108 <43392716+nolant108@users.noreply.github.com>
2020-04-28 17:19:32 -0700

    added a ton of new values

--- ML.py
+++ ML.py
@@ -1,9 +1,9 @@
 import sklearn
 from sklearn import tree
 val1 = float(input("Enter value1(10-100, x10 increments): "))
-val2 = float(input("Enter value2(1&10): "))
-features = [[10, 10], [100, 10], [20, 1], [10, 1], [ 90, 10], [ 70, 10], [40, 1], [80, 1]]
-labels = ["Hates", "Likes", "Hates", "Hates", "Likes", "Likes", "Hates", "Hates"] 
+val2 = float(input("Enter value2(1-10): "))
+features = [[1,10],[10,100], [5 ,50 ], [ 10, 60], [10, 70], [6,70], [8,90], [5,100]]
+labels = ["Hates","Likes", "Hates", "Hates", "Likes", "Likes", "Likes", "Hates"] 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 print(clf.predict([[val1, val2]]))
