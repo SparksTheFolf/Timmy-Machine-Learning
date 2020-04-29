9e8de85cf06df662f040e8c9a294185ae07cb481
nolant108 <43392716+nolant108@users.noreply.github.com>
2020-04-28 22:37:57 -0700

    Update MLOwn.py

--- MLOwn.py
+++ MLOwn.py
@@ -5,20 +5,36 @@ print('Machine Learning by Nolant108, MIT Licence Nolan Trapp (c) 2020, github.c
 
 f11 = float(input("Enter features 1-1 to be predicted: "))
 f12 = float(input("Enter features 1-2 to be predicted: "))
+l1 = input("Input Label 1 for F1-1, F1-2: ")
+
 f21 = float(input("Enter features 2-1 to be predicted: "))
 f22 = float(input("Enter features 2-2 to be predicted: "))
+l2 = input("Input Label 2 for F2-1, F2-2: ")
+
 f31 = float(input("Enter features 3-1 to be predicted: "))
 f32 = float(input("Enter features 3-2 to be predicted: "))
+l3 = input("Input Label 3 for F3-1, F3-2: ")
+
 f41 = float(input("Enter features 4-1 to be predicted: "))
 f42 = float(input("Enter features 4-2 to be predicted: "))
+l4 = input("Input Label 4 for F4-1, F4-2: ")
+
 f51 = float(input("Enter features 5-1 to be predicted: "))
 f52 = float(input("Enter features 5-2 to be predicted: "))
+l5 = input("Input Label 5 for F5-1, F5-2: ")
+
 f61 = float(input("Enter features 6-1 to be predicted: "))
 f62 = float(input("Enter features 6-2 to be predicted: "))
+l6 = input("Input Label 6 for F6-1, F6-2: ")
+
 f71 = float(input("Enter features 7-1 to be predicted: "))
 f72 = float(input("Enter features 7-2 to be predicted: "))
+l7 = input("Input Label 7 for F7-1, F7-2: ")
+
 f81 = float(input("Enter features 8-1 to be predicted: "))
 f82 = float(input("Enter features 8-2 to be predicted: "))
+l8 = input("Input Label 8 for F8-1, F8-2: ")
+
 
 print('Here is your Machine Learning inputs')
 print([f11, f12],[f21, f22] ,[f31, f32] ,[f41, f42] ,[f51, f52] ,[f61, f62] ,[f71, f72] , [f81, f82])
@@ -27,14 +43,6 @@ features = [[f11, f12],[f21, f22] ,[f31, f32] ,[f41, f42] ,[f51, f52] ,[f61, f62
 
 print('Now create your labes for the pairs(Ex: f1-1, f1-2 = Likes && f2-1, f2-2 = Hates: )')
 
-l1 = input("Input Label 1 for F1-1, F1-2: ")
-l2 = input("Input Label 2 for F2-1, F2-2: ")
-l3 = input("Input Label 3 for F3-1, F3-2: ")
-l4 = input("Input Label 4 for F4-1, F4-2: ")
-l5 = input("Input Label 5 for F5-1, F5-2: ")
-l6 = input("Input Label 6 for F6-1, F6-2: ")
-l7 = input("Input Label 7 for F7-1, F7-2: ")
-l8 = input("Input Label 8 for F8-1, F8-2: ")
 
 print('Here is your Machine Learning labels')
 print([ l1, l2, l3, l4, l5, l6, l7, l8])
