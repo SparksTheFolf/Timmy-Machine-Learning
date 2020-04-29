a4771fcc9425ade1e514839034df3605848132d5
nolant108 <43392716+nolant108@users.noreply.github.com>
2020-04-28 18:20:57 -0700

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,8 +1,8 @@
 import sklearn
 from sklearn import tree
-val1 = float(input("Enter value1(10-100, x10 increments): "))
-val2 = float(input("Enter value2(1-10): "))
-features = [[1,10],[10,100], [5 ,50 ], [ 10, 60], [10, 70], [6,70], [8,90], [5,100]]
+val1 = float(input("Enter Toy Size Value(1-10): "))
+val2 = float(input("Enter Toy Complexity Value(1-10): "))
+features = [[1,1],[10,10], [5 ,5 ], [ 10, 6], [10, 7], [6,7], [8,9], [5,10]]
 labels = ["Hates","Likes", "Hates", "Hates", "Likes", "Likes", "Likes", "Hates"] 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
